b3bf2ee79ecb487cfbf046801822d1e7ba708662
ssseasonnn <ssseasonnn@gmail.com>
2022-01-28 16:53:58 +0800

    init

--- app/src/main/java/zlc/season/desolatordemo/MainActivity.kt
+++ app/src/main/java/zlc/season/desolatordemo/MainActivity.kt
@@ -1,11 +1,38 @@
 package zlc.season.desolatordemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.io.File
+import dalvik.system.DexClassLoader
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnHello = findViewById<Button>(R.id.btn_hello)
+        btnHello.setOnClickListener {
+//            startActivity(Intent(this, TestActivity::class.java))
+
+            val dir = getDir("plugins", MODE_PRIVATE)
+            val pluginFile = File(dir, "pluginb-release-unsigned.apk")
+            pluginFile.unzip(dir.path)
+
+            val libPath = dir.path + "/classes.dex"
+            val tmpDir = getDir("dex", MODE_PRIVATE)
+            val classloader = DexClassLoader(libPath, tmpDir.absolutePath, null, this.javaClass.classLoader)
+            val classToLoad = classloader.loadClass("zlc.season.pluginb.BPluginActivity") as Class<Any>
+
+            startActivity(Intent(this, classToLoad))
+//            val myInstance = classToLoad.newInstance()
+//            val doSomething = classToLoad.getMethod("doSomething")
+
+//            doSomething.invoke(myInstance)
+
+
+        }
     }
 }

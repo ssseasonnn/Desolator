9320e0d3f125cb6905fefa11ae96e4ca7278d120
ssseasonnn <ssseasonnn@gmail.com>
2022-03-02 11:29:10 +0800

    commit

--- modules/bar/src/main/java/zlc/season/bar/BarActivity.kt
+++ modules/bar/src/main/java/zlc/season/bar/BarActivity.kt
@@ -2,10 +2,12 @@ package zlc.season.bar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import zlc.season.bar.databinding.ActivityBarBinding
 
 class BarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bar)
+        val binding = ActivityBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

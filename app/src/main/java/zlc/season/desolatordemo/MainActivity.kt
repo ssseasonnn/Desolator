8f499c358110308f27d16df4dd08f307bad08b81
ssseasonnn <ssseasonnn@gmail.com>
2022-09-09 14:08:56 +0800

    commit

--- app/src/main/java/zlc/season/desolatordemo/MainActivity.kt
+++ app/src/main/java/zlc/season/desolatordemo/MainActivity.kt
@@ -1,7 +1,6 @@
 package zlc.season.desolatordemo
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import zlc.season.desolator.Desolator
 import zlc.season.desolator.PluginData
@@ -15,46 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
-        binding.btnPluginFoo.setOnClickListener {
+        binding.btnLoadLocalPlugin.setOnClickListener {
             Desolator.startPlugin(
                 PluginData(
-                    "101574",
-                    "foo",
-                    "1",
-                    "zlc.season.foo.FooFragment"
+                    id = "101",
+                    name = "foo",
+                    version = "1",
+                    entrance = "zlc.season.foo.FooFragment"
                 )
             )
         }
 
-        binding.btnPluginBar.setOnClickListener {
-            Desolator.startPlugin(
-                PluginData(
-                    "97299",
-                    "bar",
-                    "1",
-                    "zlc.season.bar.BarFragment"
-                )
-            )
-        }
-
-
-        binding.btnPluginFromNetwork.setOnClickListener {
+        binding.btnLoadNetworkPlugin.setOnClickListener {
             val pluginData = PluginData(
-                "101574",
-                "foo",
-                "2",
-                "zlc.season.foo.FooFragment",
+                id = "101",
+                name = "foo",
+                version = "2",
+                entrance = "zlc.season.foo.FooFragment",
                 downloadUrl = "http://192.168.0.10:8000/plugin_foo/foo.apk"
             )
 
             Desolator.downloadPlugin(pluginData)
-//            Desolator.installPlugin(
-//                pluginData,
-//                onSuccess = {
-//                    Desolator.startPlugin(pluginData)
-//                }
-//            )
         }
     }
 }
